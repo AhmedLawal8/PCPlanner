4bc3b3913cd0543a6d43a34333b21194c74918e1
lowwcrystal <cl4625@columbia.edu>
2026-07-10 01:34:13 -0400

    added routes + auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,15 +7,37 @@ import { GuidesPage } from './components/GuidesPage'
 import { BuildsPage } from './components/BuildsPage'
 import { SignInPage } from './components/SignInPage'
 import { SignUpPage } from './components/SignUpPage'
+import { RequireAuth } from './context/AuthContext'
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/build" element={<QuizPage />} />
-        <Route path="/home" element={<BuildsPage />} />
-        <Route path="/summary" element={<SummaryPage />} />
+        <Route
+          path="/build"
+          element={
+            <RequireAuth>
+              <QuizPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <RequireAuth>
+              <BuildsPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/summary"
+          element={
+            <RequireAuth>
+              <SummaryPage />
+            </RequireAuth>
+          }
+        />
         <Route path="/guides" element={<GuidesPage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
